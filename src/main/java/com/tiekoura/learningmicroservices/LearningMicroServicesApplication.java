6a77c4ad39b038d3c43ac357f3fea47ceea3233f
tiekoura8 <herwaxcoulibaly1@gmail.com>
2025-03-16 18:36:31 +0000

    init micro services

---
+++ src/main/java/com/tiekoura/learningmicroservices/LearningMicroServicesApplication.java
@@ -0,0 +1,13 @@
+package com.tiekoura.learningmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningMicroServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningMicroServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tiekoura/learningmicroservices/LearningMicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiekoura.learningmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningMicroServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
